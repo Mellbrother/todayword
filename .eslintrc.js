6be8b3bba328074a82524be910430d49563312f0
Keisuke Nemura <keisuke0907@outlook.jp>
2021-06-03 19:16:50 +0900

    display 'hello world'

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jquery: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "none",
+      },
+    ],
+    "no-console": ["off"],
+  },
+};
