069dfad60e0eae00eab47d7152b01c7eed9bf98a
Keisuke Nemura <keisuke0907@outlook.jp>
2021-06-04 00:17:37 +0900

    add word data

--- app.js
+++ app.js
@@ -2,27 +2,58 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const fs = require("fs");
 
 let stack_hint = [];
-let theme = [
-  "カレー",
-  "ラーメン",
-  "ルパン",
-  "コナン",
-  "映画",
-  "ハワイ",
-  "ジャマイカ",
-  "リコーダー",
-  "教室",
-  "サンバ",
-  "プログラム",
-];
+let theme = [];
+
+var text = fs.readFileSync("words.txt", "utf8");
+var lines = text.toString().split("\n");
+for (var line of lines) {
+  theme.push(line);
+}
 
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html"); //　HTMLファイルを使う
 });
+
+app.get("/getTheme", function (req, res) {
+  let result = [];
+  // console.log(req.query.theme_num);
+  // console.log(parseInt(req.query.theme_num));
+  const theme_num = parseInt(req.query.theme_num);
+  // console.log(theme_num);
+  const random_set = [];
+
+  count = 0;
+  while (count < theme_num) {
+    let random = getRandomInt(0, theme.length);
+    if (random_set.includes(random)) {
+      continue;
+    }
+    random_set.push(random);
+    count++;
+  }
+
+  if (theme_num > theme.length) {
+    res.send("Hello World");
+  }
+  // console.log(random_set);
+
+  random_set.forEach((element) => {
+    result.push(theme[element]);
+  });
+  // console.log(result);
+  res.send(result); //　HTMLファイルを使う
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
@@ -39,7 +70,7 @@ io.on("connection", (socket) => {
 
   socket.on("display_stack", (msg) => {
     for (let i = 0; i < stack_hint.length; i++) {
-      io.emit("chat message", stack_hint[i]);
+      io.emit("hint message", stack_hint[i]);
     }
   });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -24,6 +24,41 @@ function displayStack() {
 
 socket.on("chat message", function (msg) {
   const item = document.createElement("li");
+  item.classList.add("text-info");
   item.innerText = msg;
   document.getElementById("messages").appendChild(item);
+});
+
+socket.on("hint message", function (msg) {
+  const item = document.createElement("li");
+  item.innerText = msg;
+  document.getElementById("messages").appendChild(item);
+});
+
+$("#getTheme").on("click", function () {
+  $("#result").text("通信中...");
+
+  const themeNum = document.getElementById("theme_num").value;
+
+  // Ajax通信を開始
+  $.ajax({
+    url: "/getTheme/?theme_num=" + themeNum,
+    type: "GET",
+    timeout: 5000,
+  })
+    .done(function (data) {
+      $("#result").text("");
+      document.getElementById("theme").innerHTML = "";
+
+      for (let i = 0; i < data.length; i++) {
+        const item = document.createElement("li");
+        item.classList.add("list-group-item");
+        item.innerText = data[i];
+        document.getElementById("theme").appendChild(item);
+      }
+      // 通信成功時の処理を記述
+    })
+    .fail(function () {
+      // 通信失敗時の処理を記述
+    });
 });
