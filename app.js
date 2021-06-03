44996a62f5674ca759ca14b0e7f807dad049c3e5
Keisuke Nemura <keisuke0907@outlook.jp>
2021-06-03 19:29:25 +0900

    receive chat

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const app = require("express")();
 const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", function (req, res) {
-  res.send("hello world");
+  res.sendFile(__dirname + "/static/html/index.html"); //　HTMLファイルを使う
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+  });
 });
 
 http.listen(3000, function () {
